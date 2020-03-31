a0054fd15e9e86b7b0c14c5d1c3f2fe45e3b1649
Vivo Milo C. Crame <vmc08@yahoo.com>
2020-03-31 15:19:41 +0800

    update(app): replace braft-editor with more configurable rich text editor

--- src/components/AppTextEditor/AppTextEditor.tsx
+++ src/components/AppTextEditor/AppTextEditor.tsx
@@ -1,19 +1,75 @@
-import React, { useState, useEffect } from 'react';
-import BraftEditor, { EditorState } from 'braft-editor';
+import React, { useState } from 'react';
+import cx from 'classnames';
 import styled from 'styled-components';
-import 'braft-editor/dist/index.css';
+import { Icon, Tooltip, Typography } from 'antd';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState, convertToRaw, ContentState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-const StyledBraftEditor = styled(BraftEditor)`
-  .bf-controlbar  {
-    box-shadow: none;
+const { Text } = Typography;
+
+const StyledEditorWrapper = styled.div`
+  .identifi-toolbar {
+    border: none;
+    padding: 6px !important;
+    margin-bottom: 0;
+    .toolbar-button {
+      width: 36px;
+      height: 36px;
+      color: #6a6f7b;
+      border-radius: 4px;
+      &:hover {
+        cursor: pointer;
+        background: #e8e8e880;
+      }
+    }
   }
-  .bf-content {
+  .identifi-editor {
+    font-size: 16px;
     border: 1px solid #D9D9D9;
     border-radius: 4px;
+    padding: 12px;
     min-height: 165px;
+    line-height: 1.5;
     height: auto;
+    .public-DraftStyleDefault-block {
+      margin: 0;
+    }
   }
 `;
+
+const TOOLBAR_ALIAS: { [key: string]: string | undefined } = {
+  'unordered': 'unordered-list',
+  'ordered': 'ordered-list',
+};
+
+const ToolbarComponent: React.FC<any> = (props) => {
+  const { config, onChange, currentState } = props;
+  return config.options.map((option: string, idx: number) => (
+    <Tooltip
+      key={`${option}-${idx}`}
+      placement="bottom"
+      title={(
+        <Text className="text-capitalize text-white">
+          {TOOLBAR_ALIAS[option]?.split('-').join(' ') || option}
+        </Text>
+      )}
+    >
+      <div
+        className={cx({
+          'toolbar-button d-flex align-items-center justify-content-center': true,
+          'text-primary': currentState[option] || currentState.listType === option,
+        })}
+        onClick={() => onChange(option, !currentState[option])}
+        style={{ marginRight: 2 }}
+      >
+        <Icon type={TOOLBAR_ALIAS[option] || option} />
+      </div>
+    </Tooltip>
+  ))
+}
 
 interface IAppTextEditor {
   value: string,
@@ -22,33 +78,42 @@ interface IAppTextEditor {
   placeholder?: string,
 }
 
-const AppTextEditor: React.FC<IAppTextEditor> = ({ value, onChange, disabled, placeholder = "Write something here..." }) => {
-  const [editorState, setEditorState] = useState<EditorState | null>(BraftEditor.createEditorState(value));
+const AppTextEditor: React.FC<IAppTextEditor> = ({
+  value, disabled, onChange, placeholder = "Write something here...",
+}) => {
+  const initialValue = htmlToDraft(value);
+  const contentState = ContentState.createFromBlockArray(initialValue.contentBlocks);
+  const editorInitialState = EditorState.createWithContent(contentState);
 
-  useEffect(() => {
-    if (!value && !editorState.isEmpty()) {
-      setEditorState(BraftEditor.createEditorState(value));
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+  const [editorState, setEditorState] = useState(editorInitialState);
 
   return (
-    <StyledBraftEditor
-      readOnly={disabled}
-      language="en"
-      value={editorState}
-      onChange={editorState => {
-        setEditorState(editorState);
-        if (!editorState.isEmpty()) {
-          onChange(editorState.toHTML());
-        } else if (editorState.isEmpty() && value) {
-          onChange('');
-        }
-      }}
-      controls={['bold', 'italic', 'underline', 'strike-through', 'list-ol']}
-      placeholder={placeholder}
-      fixPlaceholder={true}
-    />
+    <StyledEditorWrapper>
+      <Editor
+        readOnly={disabled}
+        placeholder={placeholder}
+        wrapperClassName="identifi-editor-wrapper"
+        editorClassName="identifi-editor"
+        toolbarClassName="identifi-toolbar"
+        editorState={editorState}
+        onEditorStateChange={newEditorState => {
+          const htmlString = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+          setEditorState(newEditorState);
+          onChange(htmlString);
+        }}
+        toolbar={{
+          options: ['inline', 'list'],
+          inline: {
+            options: ['bold', 'italic', 'underline', 'strikethrough'],
+            component: ToolbarComponent,
+          },
+          list: {
+            options: ['unordered', 'ordered'],
+            component: ToolbarComponent,
+          }
+        }}
+      />
+    </StyledEditorWrapper>
   );
 }
 
