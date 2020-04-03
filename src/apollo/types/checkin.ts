fa785d2576ec6d6f71eba2f2c4f2a7147e340dc2
Vivo Milo C. Crame <vmc08@yahoo.com>
2020-04-03 15:15:04 +0800

    update(app): fix profile checkin

--- src/apollo/types/checkin.ts
+++ src/apollo/types/checkin.ts
@@ -75,7 +75,6 @@ export interface ICheckinData {
   name: string,
   frequency: string
   days: string[]
-  currentCheckInDate?: Date,
   nextCheckInDate: Date,
   time: string,
   waitingTime: number,
