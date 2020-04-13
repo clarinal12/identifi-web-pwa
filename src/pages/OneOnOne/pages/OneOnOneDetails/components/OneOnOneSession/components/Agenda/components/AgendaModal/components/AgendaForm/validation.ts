9d3a926c01b4c25db5e046293946c1b8660f4733
Vivo Milo C. Crame <vmc08@yahoo.com>
2020-04-13 16:01:05 +0800

    update(app): remove react-snap; add agenda topic limit

--- src/pages/OneOnOne/pages/OneOnOneDetails/components/OneOnOneSession/components/Agenda/components/AgendaModal/components/AgendaForm/validation.ts
+++ src/pages/OneOnOne/pages/OneOnOneDetails/components/OneOnOneSession/components/Agenda/components/AgendaModal/components/AgendaForm/validation.ts
@@ -3,5 +3,6 @@ import * as Yup from 'yup';
 export const agendaFormSchema = Yup.object().shape({
   topic: Yup
     .string()
+    .max(100, 'Maximum limit is 100 characters.')
     .required("Topic is required."),
 });
