021cea506c700312127a85e249f4305fa61e19f6
Vivo Milo C. Crame <vmc08@yahoo.com>
2020-04-21 16:39:53 +0800

    update(app): remove 1-1 header title

--- src/pages/OneOnOne/OneOnOne.tsx
+++ src/pages/OneOnOne/OneOnOne.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Row, Col, Typography } from 'antd';
 
 import AppLayout from 'components/AppLayout';
 import OneOnOneTable from './components/OneOnOneTable';
 
-const { Title } = Typography;
-
 const OneOnOne = () => {
   return (
     <AppLayout>
-      <Row style={{ marginBottom: 32 }}>
-        <Col>
-          <Title level={3}>1-on-1s</Title>
-        </Col>
-      </Row>
       <OneOnOneTable />
     </AppLayout>
   );
