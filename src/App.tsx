3851cfe5e9b2fc75490dfe5cd7ae2b6d9a91038f
Al Sagarino <sagarinoclarin@gmail.com>
2020-08-16 17:17:33 +0800

    updated apollo client implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,51 @@
-import React, { PropsWithChildren } from 'react';
-import { Switch, Route, RouteComponentProps, Redirect } from 'react-router-dom';
-import routes from './config/routes';
-import FourOhFour from './pages/FourOhFour';
+import React, { PropsWithChildren, useEffect, useState } from "react";
+import { Switch, Route, RouteComponentProps, Redirect } from "react-router-dom";
+import routes from "./config/routes";
+import FourOhFour from "./pages/FourOhFour";
+import { ApolloProvider } from "react-apollo";
+import getApolloClient from "config/apolloClient";
 
-import { MessageProvider } from 'contexts/MessageContext';
+import { MessageProvider } from "contexts/MessageContext";
 
 const AppProviders: React.FC<PropsWithChildren<any>> = ({ children }) => (
-  <MessageProvider>
-    {children}
-  </MessageProvider>
+  <MessageProvider>{children}</MessageProvider>
 );
 
-const App = () => (
-  <AppProviders>
-    <Switch>
-      {routes.map(({ component: Component, path, ...rest }) => (
-        <Route
-          key={path}
-          path={path}
-          render={(props: RouteComponentProps & any) => <Component {...props} {...rest} />}
-        />
-      ))}
-      <Redirect exact from="/" to="/checkins"/>
-      <Route component={FourOhFour} />
-    </Switch>
-  </AppProviders>
-);
+const App = () => {
+  const [client, setClient] = useState(null) as any;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getApolloClient().then((newClient) => {
+      setClient(newClient);
+      setLoading(false);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <>Loading Client...</>;
+  }
+
+  return (
+    <AppProviders>
+      <ApolloProvider client={client}>
+        <Switch>
+          {routes.map(({ component: Component, path, ...rest }) => (
+            <Route
+              key={path}
+              path={path}
+              render={(props: RouteComponentProps & any) => (
+                <Component {...props} {...rest} />
+              )}
+            />
+          ))}
+          <Redirect exact from="/" to="/checkins" />
+          <Route component={FourOhFour} />
+        </Switch>
+      </ApolloProvider>
+    </AppProviders>
+  );
+};
 
 export default App;
--- src/config/apolloClient.ts
+++ src/config/apolloClient.ts
@@ -7,68 +7,91 @@ import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
 import { ApolloLink, Observable, Operation } from "apollo-link";
 import { ZenObservable } from "zen-observable-ts";
+import { CachePersistor } from "apollo-cache-persist";
 
 import introspectionQueryResultData from "../fragment-types.json";
 import { getAuthToken, isLoggedIn } from "utils/userUtils";
 import env from "config/env";
 
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData,
-});
-const cache = new InMemoryCache({ fragmentMatcher });
+const SCHEMA_VERSION = "1";
+const SCHEMA_VERSION_KEY = "apollo-schema-version";
 
-const request = async (operation: Operation) => {
-  operation.setContext({
-    ...(isLoggedIn() && {
-      headers: {
-        authorization: `Bearer ${getAuthToken()}`,
-      },
-    }),
+const getApolloClient = async () => {
+  const fragmentMatcher = new IntrospectionFragmentMatcher({
+    introspectionQueryResultData,
   });
+
+  const cache = new InMemoryCache({ fragmentMatcher });
+
+  const persistor = new CachePersistor({
+    cache,
+    storage: window.localStorage as any,
+  });
+
+  const currentVersion = window.localStorage.getItem(SCHEMA_VERSION_KEY);
+
+  if (currentVersion === SCHEMA_VERSION) {
+    await persistor.restore();
+  } else {
+    await persistor.purge();
+    window.localStorage.setItem(SCHEMA_VERSION_KEY, SCHEMA_VERSION);
+  }
+
+  const request = async (operation: Operation) => {
+    operation.setContext({
+      ...(isLoggedIn() && {
+        headers: {
+          authorization: `Bearer ${getAuthToken()}`,
+        },
+      }),
+    });
+  };
+
+  const requestLink = new ApolloLink(
+    (operation, forward) =>
+      new Observable((observer) => {
+        let handle: ZenObservable.Subscription;
+        Promise.resolve(operation)
+          .then((oper) => request(oper))
+          .then(() => {
+            handle = forward(operation).subscribe({
+              next: observer.next.bind(observer),
+              error: observer.error.bind(observer),
+              complete: observer.complete.bind(observer),
+            });
+          })
+          .catch(observer.error.bind(observer));
+
+        return () => {
+          if (handle) handle.unsubscribe();
+        };
+      })
+  );
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) console.log(graphQLErrors);
+        if (networkError) console.log(networkError);
+      }),
+      requestLink,
+      new HttpLink({
+        uri: process.env[`REACT_APP_${env}_API_URL`],
+        credentials: "same-origin",
+      }),
+    ]),
+    cache,
+    defaultOptions: {
+      watchQuery: {
+        fetchPolicy: "cache-and-network",
+      },
+      query: {
+        fetchPolicy: "network-only",
+      },
+    },
+  });
+
+  return client;
 };
 
-const requestLink = new ApolloLink(
-  (operation, forward) =>
-    new Observable((observer) => {
-      let handle: ZenObservable.Subscription;
-      Promise.resolve(operation)
-        .then((oper) => request(oper))
-        .then(() => {
-          handle = forward(operation).subscribe({
-            next: observer.next.bind(observer),
-            error: observer.error.bind(observer),
-            complete: observer.complete.bind(observer),
-          });
-        })
-        .catch(observer.error.bind(observer));
-
-      return () => {
-        if (handle) handle.unsubscribe();
-      };
-    })
-);
-
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      if (graphQLErrors) console.log(graphQLErrors);
-      if (networkError) console.log(networkError);
-    }),
-    requestLink,
-    new HttpLink({
-      uri: process.env[`REACT_APP_${env}_API_URL`],
-      credentials: "same-origin",
-    }),
-  ]),
-  cache,
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "cache-and-network",
-    },
-    query: {
-      fetchPolicy: "network-only",
-    },
-  },
-});
-
-export default client;
+export default getApolloClient;
