83d4e3f2de55c0984d3589924cb33598af2ddd01
Al Sagarino <sagarinoclarin@gmail.com>
2020-08-16 15:47:57 +0800

    fix typescript error for moment-timezone

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@ AppRenderer(
   rootElement
 );
 
-// If you want your app to work offline and load faster, you can change
+// If you want your app to work offline and load faster, you can chasnge
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
